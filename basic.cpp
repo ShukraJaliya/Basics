09db163878d77e9ba0f095f96bdf24f4c2dee043
Shukra <97289034+ShukraJaliya@users.noreply.github.com>
2023-12-14 03:04:01 -0800

    variables, if statements

---
+++ basic.cpp
@@ -0,0 +1,24 @@
+string name = ?;
+
+
+    //enter you age 
+    int age = ?;
+
+
+    //enter a letter 
+    char letter = ?
+
+    bool check = true;
+
+
+
+if(age > ? ){
+        cout << ? << endl;
+    }
+    else if (age == ?){
+        cout<< ? <<endl;
+    }
+    else {
+        cout << ? << endl;
+    }
+    return 0;
