5ad2043c4aff366d1fb08b96d1fe7c5246115587
Shukra <97289034+ShukraJaliya@users.noreply.github.com>
2023-12-14 04:26:36 -0800

    practice

---
+++ more.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    /////////finding summ
+    // int muArray[]={2,4,6,8,10};
+
+    //Declare array
+    //?
+
+
+
+
+
+    // int size = sizeof(muArray)/sizeof(muArray[0]);
+    // int sum=0;
+    // for (int i=0; i<size; i++){
+    //     cout  << muArray[i]<< endl;
+    //     sum += muArray[i];
+    // }
+    //  cout << sum << endl;
+}
